ab5ba6aa5dea09e26d994719a81eecd536353fed
SyedMahmood <syed.mahmood@rutgers.edu>
2016-11-20 00:56:02 -0500

    Main changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import util.FileHandler;
+import view.AdminView;
 import view.AlbumView;
 import view.LoginView;
 import javafx.scene.Scene;
@@ -16,12 +17,12 @@ public class Main extends Application
 {
 	private Stage primaryStage;
 	
-	private FileHandler fileHandler = new FileHandler("usernames.txt");
-	
 	private static String loginViewFileName = "LoginView.fxml";
 	private static String albumViewFileName = "AlbumView.fxml";
+	private static String adminViewFileName = "AdminView.fxml";
 	private LoginView loginView;
 	private AlbumView albumView; 
+	private AdminView adminView;
 	
 	@Override
 	public void start(Stage primaryStage) 
@@ -32,6 +33,7 @@ public class Main extends Application
 			
 			this.loginView = new LoginView(loginViewFileName);
 			this.albumView = new AlbumView(albumViewFileName);
+			this.adminView = new AdminView(adminViewFileName);
 			
 			addClickHandlerForLoginView();
 			
@@ -51,25 +53,17 @@ public class Main extends Application
 			@Override
 			public void handle( ActionEvent e) 
 			{
-				try {
-					if (fileHandler.doesUserNameExist(loginView.getUsername()))
-					{
-						switchToAlbumView();
-					} 
-					else 
-					{
-						//TODO: error message
-					}
-				} catch (IOException e1) {
-					e1.printStackTrace();
+				if (loginView.getUsername().equals("admin"))
+				{
+					switchToAdminView();
 				}
 			}
 		});
 	}
 	
-	public void switchToAlbumView()
+	public void switchToAdminView()
 	{
-		this.primaryStage.setScene(this.albumView.getScene());
+		this.primaryStage.setScene(this.adminView.getScene());
 	}
 	
 	public static void main(String[] args) 
